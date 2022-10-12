6d350c5d867dd7fde418662706725adfb67931b3
nasehafarooqui <nasehafarooqui10@gmail.com>
2022-10-12 07:56:06 +0530

    save changes

---
+++ java-script-practice/jspractice.js
@@ -0,0 +1,12 @@
+function myfunction(){
+    document .getElementById("demo2").innerHTML="dynamic paragraph created by javascript"
+    document .getElementById("demo2").style.backgroundColor="red"
+}
+function myfunction(){
+    document .getElementById("demo3").innerHTML="display my paragraph"
+    document .getElementById("demo3").style.backgroundColor="red"}
+
+    function myfuction() {
+        document.getElementById("demo3").innerHTML = "third button"
+        document.getElementById("demo3").style.backgroundColor = "green"}
+    
